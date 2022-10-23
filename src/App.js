1f36f99be3262d1b9a7f58dfe97ddd0899e1a0a5
Rohan Kulkarni <rohank2502@gmail.com>
2022-10-23 10:50:15 +0530

    fixed import and footer errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
-import Home from './Pages/Home';
-import Page from './Pages/Page';
-import Footer from './Pages/Footer';
+import React from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import Page from "./Pages/Page";
+import Footer from "./Pages/Footer";
 
 function App() {
   return (
